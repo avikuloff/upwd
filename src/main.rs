8257feaf7da1cb53aedb155aed950cf52ca908b4
Andrey Vikulov <avikuloff@yandex.ru>
2020-09-09 20:34:31 +0300

    Added messages to assertions. Added tests.

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,9 @@ fn main() {
     let opts: Opts = Opts::parse();
 
     let pool = opts.charset.collect();
+
+    // Если в команде передана опция --entropy, то вычисляем необходимую длину пароля,
+    // иначе присваивается значение по умолчанию.
     let length = opts.entropy.map_or(opts.length, |e| {
         calculate_length(e, pool.len() as f64).ceil() as usize
     });
@@ -90,16 +93,16 @@ impl Charset {
             pool.extend(Charset::SYMBOLS.chars());
         }
 
-        assert!(!pool.is_empty());
+        assert!(!pool.is_empty(), "Pool contains no elements!");
 
         pool
     }
 }
 
-/// Generate random password.
-/// Panics if `pool` is empty.
+// Generate random password.
+// Panics if `pool` is empty.
 fn generate_password(pool: IndexSet<char>, length: usize) -> String {
-    assert!(!pool.is_empty());
+    assert!(!pool.is_empty(), "Pool contains no elements!");
 
     let mut rng = rand::thread_rng();
 
@@ -111,16 +114,17 @@ fn generate_password(pool: IndexSet<char>, length: usize) -> String {
         .collect()
 }
 
-/// Calculates entropy.
+// Calculates entropy.
+// ToDo Нужно ли возвращать бесконечность?
 fn calculate_entropy(length: u32, pool_size: usize) -> f64 {
     BigUint::from(pool_size)
         .pow(length)
         .to_f64()
-        .expect("Typecast error! Failed to convert BigUint to f64.")
+        .expect("Typecast error! Failed to convert BigUint to f64!")
         .log2()
 }
 
-/// Calculates the required password length to obtain the given entropy.
+// Calculates the required password length to obtain the given entropy.
 fn calculate_length(entropy: f64, pool_size: f64) -> f64 {
     entropy / pool_size.log2()
 }
@@ -133,7 +137,7 @@ struct Info {
 }
 
 impl Info {
-    /// Creates new instance
+    // Creates new instance
     fn new(entropy: f64, length: usize, pool_size: usize) -> Self {
         Info {
             entropy,
@@ -142,11 +146,110 @@ impl Info {
         }
     }
 
-    /// Prints info
+    // Prints info
     // FixMe Как обработать ошибки?
     fn write(&self, mut writer: impl Write) {
         writeln!(writer, "Entropy: {:.0} bits", self.entropy).unwrap();
         writeln!(writer, "Length: {} chars", self.length).unwrap();
         writeln!(writer, "Pool size: {} chars", self.pool_size).unwrap();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn charset_collect() {
+        let charset = Charset {
+            uppercase: true,
+            lowercase: true,
+            digits: true,
+            symbols: true,
+        }.collect();
+
+        assert!(charset.contains(&'A'));
+        assert!(charset.contains(&'a'));
+        assert!(charset.contains(&'0'));
+        assert!(charset.contains(&'&'));
+    }
+
+    #[test]
+    #[should_panic(expected = "Pool contains no elements!")]
+    fn charset_collect_all_fields_false() {
+        Charset {
+            uppercase: false,
+            lowercase: false,
+            digits: false,
+            symbols: false,
+        }.collect();
+    }
+
+    #[test]
+    fn generate_password_assert_len() {
+        let pool = "0123456789".chars().collect::<IndexSet<char>>();
+        let password = generate_password(pool, 15);
+
+        assert_eq!(password.len(), 15);
+    }
+
+    #[test]
+    #[should_panic(expected = "Pool contains no elements!")]
+    fn generate_password_passed_empty_pool() {
+        let pool = "".chars().collect::<IndexSet<char>>();
+
+        generate_password(pool, 15);
+    }
+
+    #[test]
+    fn calculate_entropy_assert_true() {
+        let entropy = calculate_entropy(12, 64);
+
+        assert_eq!(entropy, 72.0);
+    }
+
+    #[test]
+    fn calculate_entropy_passed_length_is_0() {
+        let entropy = calculate_entropy(0, 64);
+
+        assert_eq!(entropy, 0.0)
+    }
+
+    #[test]
+    fn calculate_entropy_passed_pool_size_is_0() {
+        let entropy = calculate_entropy(12, 0);
+
+        assert_eq!(entropy, f64::NEG_INFINITY)
+    }
+
+    #[test]
+    fn calculate_length_assert_true() {
+        let length = calculate_length(128.0, 64.0);
+
+        assert_eq!(length.ceil(), 22.0);
+    }
+
+    #[test]
+    fn calculate_length_passed_entropy_is_0() {
+        let length = calculate_length(0.0, 64.0);
+
+        assert_eq!(length.ceil(), 0.0);
+    }
+
+    #[test]
+    fn calculate_length_pool_size_is_0() {
+        let length = calculate_length(128.0, 0.0);
+
+        assert_eq!(length, 0.0);
+    }
+
+    #[test]
+    fn info_write() {
+        let mut actual: Vec<u8> = vec![];
+        Info::new(64.0, 15, 64).write(&mut actual);
+
+        let expected = b"Entropy: 64 bits\nLength: 15 chars\nPool size: 64 chars\n".to_vec();
+
+        assert_eq!(actual, expected);
     }
 }
