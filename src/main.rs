dcbf2225b5ed62f5f53d442831d65a92f13f162b
Andrey Vikulov <avikuloff@yandex.ru>
2020-11-08 11:13:09 +0300

    Improved handling of --config flag

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,14 @@ use upwd::{calculate_length, generate_password};
 fn main() {
     let opts: Cli = Cli::parse();
 
-    // Todo обработать ошибки, добавить сообщение и завершить выполнение
     if opts.reset() {
-        Config::save_default().unwrap();
+        match Config::save_default() {
+            Ok(_) => {
+                println!("The default configuration was set successfully!");
+                std::process::exit(exitcode::OK);
+            }
+            Err(e) => eprintln!("{}", e.to_string())
+        }
     }
 
     let pool = opts.collect();
